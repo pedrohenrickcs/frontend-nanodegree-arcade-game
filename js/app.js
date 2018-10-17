e2180935471e11740bc9741d14a39686f1653159
Pedro Henrick <pedro.henrickcs@gmail.com>
2018-10-16 22:26:57 -0300

    continued project

--- js/app.js
+++ js/app.js
@@ -1,40 +1,60 @@
 // Inimigos que nosso jogador deve evitar
-var Enemy = function() {
+
+class Enemy {
+    
     // As variáveis aplicadas a nossas instâncias entram aqui.
     // Fornecemos uma a você para que possa começcar.
+
+    // Atualize a posição do inimigo, método exigido pelo jogo
+    // Parâmetro: dt, um delta de tempo entre ticks
+    update(dt) {
+        
+        // Você deve multiplicar qualquer movimento pelo parâmetro
+        // dt, o que garantirá que o jogo rode na mesma velocidade
+        // em qualquer computador.
+    };
 
     // A imagem/sprite de nossos inimigos, isso usa um
     // ajudante que é fornecido para carregar imagens
     // com facilidade.
-    this.sprite = 'images/enemy-bug.png';
-    console.log(this);
-    
-};
-
-// Atualize a posição do inimigo, método exigido pelo jogo
-// Parâmetro: dt, um delta de tempo entre ticks
-Enemy.prototype.update = function(dt) {    
-    // Você deve multiplicar qualquer movimento pelo parâmetro
-    // dt, o que garantirá que o jogo rode na mesma velocidade
-    // em qualquer computador.
-};
-
-// Desenhe o inimigo na tela, método exigido pelo jogo
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // Desenhe o inimigo na tela, método exigido pelo jogo
+    render(sprite) {
+        sprite = 'images/enemy-bug.png'
+        console.log('render');    
+        ctx.drawImage(Resources.get(sprite), 30, 60);
+    };
 };
 
 // Agora, escreva sua própria classe de jogador
 // Esta classe exige um método update(), 
 // um render() e um handleInput().
-var player = function () {
-    update();
+class Player {
+    
+    constructor(sprite = 'images/enemy-bug.png') {
+        console.log('sprite', sprite);   
+    }
+    
+    update() {
+        console.log('update');        
+    }
+
+    // render() {
+    //     console.log('render');    
+    // }
+
+    // handleInput() {
+    //     console.log('handleInput');        
+    // }
 };
+
+const player = new Player();
 
 // Represente seus objetos como instâncias.
 // Coloque todos os objetos inimgos numa array allEnemies
 // Coloque o objeto do jogador numa variável chamada jogador.
-var allEnemies = [];
+var allEnemies = [
+    new Enemy(0,0)
+];
 
 // Isto reconhece cliques em teclas e envia as chaves para seu
 // jogador. método handleInput(). Não é preciso mudar nada.
